5cf68371b2ca0ff76df0bedd83a4e68b7c3648c6
Daved892 <125751083+Daved892@users.noreply.github.com>
2023-05-11 12:23:25 -0500

    Add files via upload

---
+++ sumaCR.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float a,b,c;
+	cout<<"ingrese a b: "<<a<<b;
+	c=a+b;
+	cout<<"la suma de "<<a<<" +"<<b<<" = "<<c<<endl;
+	
+
+
+	return 0;
+
+
+}
